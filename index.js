da97500ae6a96ff65f75f95399b0974a34bedb12
Sagar <70952463+Sagar7777521@users.noreply.github.com>
2024-03-03 17:02:44 +0530

    Bind Action

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const redux = require("redux");
 
 const createStore = redux.createStore;
+const bindActionCreatore = redux.bindActionCreators;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
@@ -12,7 +13,7 @@ function orderCake() {
   };
 }
 
-function restockCake(qty = 1) {
+function restockCake(qty) {
   return {
     type: CAKE_RESTOCKED,
     payload: qty,
@@ -46,9 +47,16 @@ const unsubscribe = store.subscribe(() =>
   console.log("Updated State", store.getState())
 );
 
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(restockCake(10));
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(restockCake(1));
+
+const actions = redux.bindActionCreators(
+  { orderCake, restockCake },
+  store.dispatch
+);
+actions.orderCake();
+actions.restockCake(5);
 
 unsubscribe();
